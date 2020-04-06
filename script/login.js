b039f605b9ccf9eba35829a184e31f4368ec6b61
Gurapun51 <59070097@it.kmitl.ac.th>
2020-04-06 21:30:35 +0700

    up index page

---
+++ script/login.js
@@ -0,0 +1,27 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+      var user = firebase.auth().currentUser;
+      if (user != null) {
+          window.location.replace("home.html");
+      }
+  } 
+});
+
+function login(){
+  let email = document.getElementById("email").value
+  let password = document.getElementById("password").value
+  console.log(email)
+  console.log(password)
+  firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/wrong-password') {
+      window.alert('Wrong password.');
+    } else {
+      window.alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
---
+++ script/register.js
@@ -0,0 +1,45 @@
+function register() {
+  let username = document.getElementById("username").value
+  let password = document.getElementById("password").value
+  let confirmpassword = document.getElementById("confirmpassword").value
+  let email = document.getElementById("email").value
+  var errorMessage = ""
+  var errorCode = ""
+  console.log(username)
+  console.log(password)
+  console.log(confirmpassword)
+  console.log(email)
+  if (password == confirmpassword) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert(errorMessage)
+        // ...
+      });
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var user = firebase.auth().currentUser;
+            
+            if (user != null) {
+                let uid = user.uid
+                console.log(uid)
+                console.log(username)
+                firestore.collection("user").doc("66666666").set({
+                  username: "username",
+                  password: "password",
+                  email: "email"
+                });
+                firebase.auth().signOut().then(function() {
+                // Sign-out successful.
+                // window.location.replace("login.html");
+                }).catch(function(error) {
+                    // An error happened.
+                });
+            }
+        } 
+      });
+}else{
+      window.alert("password not math")
+  }
+}
